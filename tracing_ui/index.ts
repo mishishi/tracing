fafd223406a5c3bfe3f3cc40058cddd256bca2f8
Test User <test@example.com>
2026-06-02 14:13:01 +0800

    chore: remove unused tracing_ui (superseded by tracing-dashboard)

--- tracing_ui/index.ts
+++
@@ -1,3 +0,0 @@
-export { TraceViewer } from "./src/TraceViewer";
-export { Dropdown } from "./src/Dropdown";
-export type { Span, SpanMeta, TraceData, TraceSummary, Stats } from "./src/TraceViewer";
